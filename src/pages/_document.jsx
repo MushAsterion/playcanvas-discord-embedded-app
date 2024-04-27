70ca43da84a05147c751800e8e8b6d6a39224df7
Lucas Petitjean <28061629+MushAsterion@users.noreply.github.com>
2024-04-27 20:37:17 +0100

    Added base for Playcanvas Game embedding

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,10 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="stylesheet" type="text/css" href="styles.css" />
+                <script src="playcanvas-stable.min.js"></script>
+                <script src="__settings__.js"></script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
+                <script src="__modules__.js"></script>
             </body>
         </Html>
     );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,36 @@
+import Head from 'next/head';
+import { useEffect } from 'react';
+
+function addScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.defer = true;
+
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', reject);
+        document.body.appendChild(script);
+        return script;
+    });
+}
+
 export default function Home() {
-    return <>Hello World</>;
+    useEffect(() => {
+        (async () => {
+            document.body.style.backgroundColor = '#000';
+
+            await addScript('__start__.js');
+            await addScript('__loading__.js');
+        })();
+    }, []);
+
+    return (
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover" />
+            <meta charSet="utf-8" />
+            <link rel="manifest" href="manifest.json" />
+            <style></style>
+            <title>Discord Roll a ball</title>
+        </Head>
+    );
 }
